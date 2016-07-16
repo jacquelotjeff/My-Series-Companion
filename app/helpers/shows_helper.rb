f98989e54bfad2e2bd8681269af39ea2fa5d8959
Jonathan Cholet <choletjonathan@gmail.com>
2016-07-16 18:28:45 +0200

    many stuff

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -9,6 +9,8 @@ module ShowsHelper
 			"http://placehold.it/262x381"
 		elsif type == 'banner_episode'
 		  	"http://placehold.it/400x225"
+		 elsif type == 'new_poster'
+		  	"http://placehold.it/144x212"
 		end
   	else
   		"http://thetvdb.com/banners/#{url}"
