c4bcfbb5e9851c114a453d7e924b9b3012dfaca4
jacquelotjeff <jacquelotjeff@gmail.com>
2016-07-28 22:56:19 +0200

    passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,6 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :rvm_ruby_version, '2.3.0'
 
-require "rvm/capistrano"
-
 #set :passenger_environment_variables, { :path => '/path-to-passenger/bin:$PATH' }
 #set :passenger_restart_command, '/path-to-passenger/bin/passenger-config restart-app'
 
